cca5bebf9e5f3e3dd952889f22f8b9b4af5f6990
WebShohDev <raximovshaxzodbek2002@gmail.com>
2022-07-21 22:09:48 +0000

    Rendering Elements

--- src/components/MAIN CONCEPTS/Rendering Elements/index.jsx
+++ src/components/MAIN CONCEPTS/Rendering Elements/index.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 export default function RenderingElements() {
   return (
-    <div>RenderingElements</div>
+    <div className='text-center'>
+      <h1 className='text-danger'> 
+        {new Date().toLocaleTimeString()}
+      </h1>
+    </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -7,4 +7,29 @@ root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
+)
+// const hello = <h1>Hello World</h1>
+
+// vaqt oralig'ida jarayon hosil qilish.
+
+// function SetTimeFunction() {
+//   const element = (
+//     <div>
+//       {hello}
+//       <h1>{ new Date().toLocaleString() }</h1>
+//     </div>
+//   )
+//   root.render(element)
+// }
+
+// setInterval(SetTimeFunction, 1000 )
+// Vaqt Qiymati qo'shilganda qayta qayta renderni tekshirish jarayoni.
+// function SetTime() {
+//   root.render(
+//     <React.StrictMode>
+//       <App />
+//     </React.StrictMode>
+//   );
+// }
+
+// setInterval(SetTime, 1000)
