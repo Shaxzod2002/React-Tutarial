6d0be92cbfacdb92779f82c481d560641cfb3822
WebShohDev <raximovshaxzodbek2002@gmail.com>
2022-07-28 20:11:51 +0000

    React Components and Props

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 // Lesson 1 Papkasini Qo'shish
 // import Lesson1 from './Lessons/Lesson1'
-import Lesson2 from './Lessons/Lesson2'  
-import Lesson3 from './Lessons/Lesson3'
+// import Lesson2 from './Lessons/Lesson2'  
+// import Lesson3 from './Lessons/Lesson3'
 import Lesson4 from './Lessons/Lesson4'
 
 export default function Components() {
@@ -10,9 +10,9 @@ export default function Components() {
         <div>
             {/* Lesson ni namoyish etish */}
             {/* <Lesson1 /> */}
-            <Lesson2 />
+            {/* <Lesson2 /> */}
             {/* <Lesson3 /> */}
-            {/* <Lesson4  /> */}
+            <Lesson4  />
         </div>
     )
 }
